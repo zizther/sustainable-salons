1a01ad3fde4099a48dfc11035c13a0f97408121c
ian <witham.ian@gmail.com>
2023-03-03 21:34:11 +1300

    restore alpine cdn link in head

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,14 @@
 //import "alpinejs"; // uncomment if you want to use alpinejs
 import { gsap, Power3, Power2 } from "gsap";
-import { Observer } from "gsap/Observer"
+import { Observer } from "gsap/Observer";
 
 gsap.registerPlugin(Observer);
-
 
 const documentHeight = () => {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
 
-documentHeight()
-
-
+documentHeight();
